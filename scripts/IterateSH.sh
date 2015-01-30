e932bb8b3d8956e43fd489a049219c2f784dedc5
pschorderet <patrick.schorderet@gmail.com>
2015-01-30 14:00:26 -0500

    ChIPpip

--- scripts/IterateSH.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-FIRST=`qsub -N Iterate -o /data/schorderet/projects/EXAMPLE/scripts/qsub -e /data/schorderet/projects/EXAMPLE/scripts/qsub /data/schorderet/projects/EXAMPLE/scripts/ChIPseq.sh`
